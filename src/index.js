05911470cdc190d4e17cb2c698d50070a2f0dc21
SOWMYA <sowmyasriramula216@gmail.com>
2023-10-14 17:18:46 +0530

    Added v1 for tweet model and hashtag model

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 const express=require('express')
 const connect=require('./config/database');
-const TweetRepository = require('./repository/tweet-repository');
+
 const app=express()
-const Comment=require('./models/comment')
+
 app.listen(3000,async()=>{
     console.log('Server Started');
     await connect()
     console.log('Mongodb connected');
-    // const tweet=await Tweet.create({
-    //     content:'3rd tweet',
-       
-    // })
-    // const tweets=await Tweet.find({userEmail:'a@b.com'})
-    const tweetRepo=new TweetRepository()
-
-    const tweet=await tweetRepo.create({content:'With hooks now'})
-    console.log(tweet);
-    // const tweet=await tweetRepo.getAll(0,4)
-    // console.log(tweet[0].contentWithEmail);
-
+    
 })
